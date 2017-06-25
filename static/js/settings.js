89732a08a219c4a38dc740f18212a66d5046efd2
LiteHell <LiteHell@openmailbox.org>
2017-06-25 23:21:56 +0900

    Implement tempsave

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,3 +1,4 @@
+// TO-DO : 이거 이용해서 설정 구현.
 var Uman = (function(){
     var userSettings = {};
     this.getItem = function(key, defaultVal) {
@@ -38,3 +39,17 @@ var Uman = (function(){
 if (!window.uman)
     window.uman = Uman;
 uman.loadSettings();
+
+function deleteAllTempsaves(n) {
+    var tempsaveManager = new TempsaveManager();
+    if (n == 0) {
+        var docName = prompt('문서 이름을 입력하세요.');
+        tempsaveManager.deleteTempsaves(docName);
+        alert('완료했습니다');
+    } else if (n == 1) {
+        if(confirm('정말로 모든 임시저장을 삭제하시겠습니까?')) {
+            tempsaveManager.clearTempsaves();
+            alert('완료했습니다');
+        }
+    }
+}
